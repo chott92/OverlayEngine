7637e447ecfa29f1dd1e8dbf120d1c0e4edcec27
Christian Ott <chott@gmx.net>
2018-07-24 10:01:31 +0200

    add javascript routs for creating run informations

--- src/main/resources/static/page/resources/js/mainApp.js
+++ src/main/resources/static/page/resources/js/mainApp.js
@@ -1,39 +1,44 @@
 var app = angular.module('overlayEngineApp', ['ngRoute']);
 app.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'dashboard.htm',
-                controller: 'DashboardController'
-            })
+	function ($routeProvider) {
+		$routeProvider
+				.when('/', {
+					templateUrl: 'dashboard.htm',
+					controller: 'DashboardController'
+				})
 
-            .when('/runInformationOverview', {
-                templateUrl: 'runInformationOverview.htm',
-                controller: 'RunInformationOverviewController'
-            })
+				.when('/runInformationOverview', {
+					templateUrl: 'runInformationOverview.htm',
+					controller: 'RunInformationOverviewController'
+				})
 
-            .when('/runInformation', {
-                templateUrl: 'runInformation.htm',
-                controller: 'RunInformationController'
-            })
+				.when('/runInformation', {
+					templateUrl: 'runInformation.htm',
+					controller: 'RunInformationController'
+				})
 
-            .otherwise({redirectTo: '/'});
-    }]);
+				.otherwise({redirectTo: '/'});
+	}]);
 
 app.controller('DashboardController', function ($scope) {
 
 });
 
 app.controller('RunInformationOverviewController', function ($scope, $http) {
-    $scope.msg = 'Loading data...';
-    $http.get('/api/runInformation/all')
-        .then(function (res) {
-            $scope.runInformations = res.data;
-            $scope.msg = '';
-        });
+	$scope.msg = 'Loading data...';
+	$scope.test = 'hello world';
+	$http.get('/api/runInformation/all')
+			.then(function (res) {
+				$scope.runInformations = res.data;
+				$scope.msg = '';
+			});
 });
 
-app.controller('RunInformationController', function ($scope, $http) {
-    $scope.runInformationMode = 'Create';
-    $scope.runInformation = {};
+app.controller('RunInformationController', function ($scope, $http, $location) {
+	$scope.runInformationMode = 'Create';
+	$scope.runInformation = {};
+
+	$scope.saveInformation = function () {
+		$location.path('/runInformationOverview')
+	};
 });
