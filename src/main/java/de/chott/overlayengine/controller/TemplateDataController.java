dd3b806543d9a150671569c9c0ac01d88c63db08
Christian Ott <christian.ott@anevis-solutions.com>
2018-08-14 09:25:36 +0200

    add nextRun and AfterNextRun to TemplateData

--- src/main/java/de/chott/overlayengine/controller/TemplateDataController.java
+++ src/main/java/de/chott/overlayengine/controller/TemplateDataController.java
@@ -3,6 +3,8 @@ package de.chott.overlayengine.controller;
 import de.chott.overlayengine.model.database.RunInformation;
 import de.chott.overlayengine.model.template.TemplateData;
 import de.chott.overlayengine.service.RunDataServiceMockup;
+import de.chott.overlayengine.service.database.RunInformationService;
+import java.util.List;
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,11 +13,13 @@ import org.springframework.stereotype.Component;
 public class TemplateDataController {
 
 	private final TemplateData currentData;
+	private final RunInformationService runInformationService;
 
 	@Autowired
-	public TemplateDataController(RunDataServiceMockup mockup) {
+	public TemplateDataController(RunDataServiceMockup mockup, RunInformationService runInformationService) {
 		currentData = new TemplateData();
 		currentData.setCurrentRun(mockup.getNextInformation());
+		this.runInformationService = runInformationService;
 	}
 
 	public TemplateData getCurrentData() {
@@ -24,6 +28,17 @@ public class TemplateDataController {
 
 	public void setCurrentRun(RunInformation information) {
 		currentData.setCurrentRun(information);
+
+		List<RunInformation> followingRuns =
+				runInformationService.findRunsAfterOrderIndex(information.getOrderIndex());
+
+		if (followingRuns.size() > 0) {
+			currentData.setNextRun(followingRuns.get(0));
+		}
+		if (followingRuns.size() > 1) {
+			currentData.setAfterNextRun(followingRuns.get(1));
+		}
+
 		Logger.getLogger(this.getClass().getName()).info("Changed the run to " + information.getGame());
 	}
 }
