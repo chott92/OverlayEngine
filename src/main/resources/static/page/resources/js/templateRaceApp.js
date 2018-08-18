e8690e229aba7a7a3b708981905e3444fc4f2783
Christian Ott <chott@gmx.net>
2018-08-18 11:01:42 +0200

    add race template, template file and javascript app

---
+++ src/main/resources/static/page/resources/js/templateRaceApp.js
@@ -0,0 +1,59 @@
+var templateApp = {
+	resizeField: function (target) {
+		if (document.getElementById(target)) {
+			$('#' + target).textfill();
+		}
+	},
+	loadData: function () {
+		$.get('/api/templateData', function (data) {
+			console.log(data);
+			templateApp.updateFields(data);
+		});
+	},
+	updateFields: function (data) {
+		var currentRunData = templateApp.adjustRunnerNames(data.currentRun, data.isNamesSwapped);
+		templateApp.updateField('runnerOne', currentRunData.runnerOne);
+		templateApp.updateField('runnerTwo', currentRunData.runnerTwo);
+		templateApp.updateField('estimate', currentRunData.estimate);
+		templateApp.updateField('game', currentRunData.game);
+		templateApp.updateField('runnerName', currentRunData.runnerName);
+		templateApp.updateField('category', currentRunData.category);
+		templateApp.updateField('platform', currentRunData.platform);
+	},
+	updateField: function (divId, newValue) {
+		if (document.getElementById(divId)) {
+			var target = $('#' + divId);
+			target.html('<span>' + newValue + '</span>');
+			target.textfill();
+		}
+	},
+	adjustRunnerNames: function (runData, isNamesSwapped) {
+		var splitNames = runData.runnerName.split(',');
+		if (splitNames.length < 2) {
+			splitNames = [runData.runnerName, ''];
+		}
+		if (isNamesSwapped) {
+			runData.runnerOne = splitNames[1].trim();
+			runData.runnerTwo = splitNames[0].trim();
+		} else {
+			runData.runnerOne = splitNames[0].trim();
+			runData.runnerTwo = splitNames[1].trim();
+		}
+
+		return runData;
+	}
+}
+
+$(function () {
+	templateApp.resizeField('runnerOne');
+	templateApp.resizeField('runnerTwo');
+	templateApp.resizeField('estimate');
+	templateApp.resizeField('game');
+	templateApp.resizeField('platform');
+	templateApp.resizeField('category');
+
+	templateApp.loadData();
+	setInterval(function () {
+		templateApp.loadData()
+	}, 10000);
+});
